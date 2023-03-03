30991681ee8c85f4842a56edb773ec28e2335131
Kaweesi Matia <matiakaweesi@gmail.com>
2023-03-04 00:58:22 +0300

    modify greetings model

--- src/redux/greeting.js
+++ src/redux/greeting.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const RANDOM_GREETING = 'greeting/RANDOM_GREETING';
-const baseurl = 'http://localhost:3000/greetings';
+const baseurl = 'http://127.0.0.1:3000/greetings';
 
 const initialState = [];
 
